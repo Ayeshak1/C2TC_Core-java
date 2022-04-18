0316ffe13b2e4edc05a84bf3bb6f6947b897f0ac
pyramid <pyramid@PYRAMID-PC>
2022-04-19 02:52:06 +0530

    first commit

---
+++ operstors/src/operstors/preandpostincreament.java
@@ -0,0 +1,21 @@
+package operstors;
+
+public class preandpostincreament {
+
+	public static void main(String[] args) {
+	int a=5;
+	int b=2;
+	int c;
+	int d;
+	c=++b;
+	d=a++;
+	c++;
+	System.out.println("a="+a+"b="+b+"c="+c+"d="+d);
+	
+	
+	
+	
+	
+	}
+
+}
---
+++ operstors/src/operstors/test2.java
@@ -0,0 +1,14 @@
+package operstors;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		int a=5,b=18;
+		if((a=3)==b)
+		{System.out.println(++a);}
+		else
+			
+		{System.out.println(++b);}
+	}
+
+}
