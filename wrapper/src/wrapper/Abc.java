fd14546596ce9a97c99cddb5a144a52fd2171439
pyramid <pyramid@PYRAMID-PC>
2022-04-25 07:40:51 +0530

    first commit

---
+++ wrapper/src/wrapper/Abc.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+public class Abc {
+	public static void main(String args[]) {
+		int a=20;
+		Integer i=Integer.valueOf(a);
+		Integer j=a;
+		System.out.println(a+" "+i+" "+j);
+		
+	}
+
+}
+
+
---
+++ wrapper/src/wrapper/demo.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class demo {
+
+	public static void main(String args[]) {
+		int a=20;
+		Integer i=Integer.valueOf(a);
+		Integer j=a;
+		System.out.println(a+" "+i+" "+j);
+		
+	}
+
+}
