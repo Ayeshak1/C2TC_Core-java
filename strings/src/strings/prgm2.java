5bc96da8d21f8114201c62c9eada45ce43d55769
pyramid <pyramid@PYRAMID-PC>
2022-04-25 06:37:54 +0530

    first commit

---
+++ strings/src/strings/prgm2.java
@@ -0,0 +1,27 @@
+package strings;
+
+public class prgm2 {
+	class Main{
+
+		public static void main(String args[]){
+
+		String str1 = "Hello";
+
+		String str2 = "Hello";
+
+		String s1 = new String("Hello");
+
+		String s2 = new String("Hello");
+
+		System.out.println(str1 == str2);
+
+		System.out.println(str2 == s1);
+
+		System.out.println(s1 == s2);
+
+		}
+
+		}
+
+
+}
---
+++ strings/src/strings/prgm3.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class prgm3 {
+
+	class Main{
+
+		public static void main(String args[]){
+
+		String s1 = new String("C2TC");
+
+		String s2 = new String("C2TC");
+
+		System.out.println(s1 == s2);
+
+		System.out.println(s1.equals(s2));
+
+		}
+
+		}
+
+	}
+
+
